3f48b950fb7592cba9819395572fcbdf7f22bece
Tatsiana Tylets <ttylets@mail.ru>
2023-02-05 17:18:37 +0300

    Add readme file

--- Homework_03/task_06.py
+++ Homework_03/task_06.py
@@ -1,1 +1,6 @@
 # Пользователь вводит месяц и число. Выведите True, если такой день есть в году.
+month = input('Введите месяц: ')
+date = int(input('Введите число: '))
+dict_a = {'January': 31, 'February': 28, 'March': 31, 'April': 30, 'May': 31, 'June': 30, 'July': 31,
+          'August': 31, 'September': 30, 'October': 31, 'November': 30, 'December': 31}
+print (dict_a.get(month) >= date)
---
+++ Homework_04-05/task_01_1.py
@@ -0,0 +1,3 @@
+num = 0
+for num in range(5, 101, 5):
+    print(num)
