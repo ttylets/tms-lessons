75c5970e4698f2eb98ba8a2d21a20ac7ffcaef02
Tatsiana Tylets <ttylets@mail.ru>
2023-03-26 21:44:50 +0300

    test

---
+++ Homework_13/task_01.py
@@ -0,0 +1,19 @@
+from flask import Flask
+
+
+app = Flask(__name__)
+
+
+@app.route('/ping')
+def ping():
+    return '<a href = "/pong">pong</a>'
+
+
+@app.route('/pong')
+def pong():
+    return '<a href = "/ping">ping</a>'
+
+
+if __name__ == '__main__':
+    app.run(port=8081, debug=True)
+
